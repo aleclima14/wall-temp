26fe7942dc5119829af15e66f79c4d7944aa28ab
Alecsander Lima <93883349+aleclima14@users.noreply.github.com>
2023-03-13 21:58:48 -0300

    Adicionando novas mensagens na serial para debug

--- src/main.cpp
+++ src/main.cpp
@@ -21,28 +21,34 @@ void setup()
 {
   fnvInitPins();
   fnvStartDHT22();
+  fnvStatusLed(false);
   Serial.begin(9600);
 }
 
 void loop() 
 {
-  Serial.println("Init board");
   fnvEnableDHT22(true);
   fnvStatusLed(true);
+  Serial.println("");
+  Serial.println("Init board");
+
   for(int i = 0; i < 3; i++)
   {
     temperature = fnfReadTemperatureDHT22();
     humidity = fnfReadHumidityDHT22();
     delay(1000);
+    Serial.print(".");
   }
 
+  Serial.println("");
   Serial.print("Temp: ");
-  Serial.print(temperature);
+  Serial.print(temperature, 1);
   Serial.print(" Humid: ");
-  Serial.println(humidity);
+  Serial.println(humidity, 0);
 
   fnvEnableDHT22(false);
   delay(100);
+  Serial.println("Go to sleep zzZZ");
   fnvGoToSleep(DEEP_SLEEP, MINUTES_TO_SLEEP);
 }
 
